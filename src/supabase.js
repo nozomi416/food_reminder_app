019162419d5877cadfc822f01be6276875cc53d8
n.takahashi <dkhdff.0415@gmail.com>
2025-04-02 23:38:14 +0900

    環境変数設定

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = "https://xtlqttulooidepaprifp.supabase.co";  // SupabaseのURL
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh0bHF0dHVsb29pZGVwYXByaWZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIwNjIzNTIsImV4cCI6MjA1NzYzODM1Mn0.HZjsPPbyREhJI1zLCD_i_o1pmNl40-p2SrZw0MlS2Cg";  // Supabaseの公開APIキー
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
