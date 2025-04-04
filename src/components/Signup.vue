cc995fe2480cb96d69e2044067895b8a63e9ab29
n.takahashi <dkhdff.0415@gmail.com>
2025-04-05 02:39:23 +0900

    アカウント登録画面：プレースホルダー修正

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -9,11 +9,11 @@
                     <label for="email" class="block mb-2 font-medium text-gray-900">メールアドレス</label>
                     <input v-model="email" type="email" name="email" id="email"
                         class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                        placeholder="name@company.com" required />
+                        placeholder="メールアドレス" required />
                 </div>
                 <div>
                     <label for="password" class="block mb-2 font-medium text-gray-900">パスワード</label>
-                    <input v-model="password" type="password" name="password" id="password" placeholder="••••••••"
+                    <input v-model="password" type="password" name="password" id="password" placeholder="6桁以上の英数字"
                         class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                         required />
                 </div>
