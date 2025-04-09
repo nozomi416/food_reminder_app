fae8cb8cbdddb43b2fa25d7a11e5e15e755ba192
n.takahashi <dkhdff.0415@gmail.com>
2025-04-10 01:04:50 +0900

    UI修正

--- src/components/FoodList.vue
+++ src/components/FoodList.vue
@@ -3,14 +3,17 @@
         <!-- 固定部分 -->
         <div class="fixed top-0 left-0 right-0 bg-secondary z-40 pt-19">
             <div class="flex items-center justify-between flex-column md:flex-row flex-wrap pt-1 pb-4 px-6">
-                <div class="text-stone-600">
+                <div class="text-accent">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" class="size-6">
+                        stroke="currentColor" class="size-7">
                         <path stroke-linecap="round" stroke-linejoin="round"
                             d="M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75" />
                     </svg>
                 </div>
-                <a href="#" @click.prevent="isCreateModalOpen = true" class="font-medium text-primary">追加</a>
+                <a href="#" @click.prevent="isCreateModalOpen = true"
+                    class="font-medium text-white bg-accent py-2 px-5 rounded-full flex items-center justify-center">
+                    追加
+                </a>
             </div>
 
             <ul class="w-full bg-white px-6 rounded-t-xl">
