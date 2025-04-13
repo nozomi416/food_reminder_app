bfdb89e3488857d3c825b5ba82c45e260bc3a474
n.takahashi <dkhdff.0415@gmail.com>
2025-04-13 23:23:43 +0900

    PWA対応

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,29 @@ export default defineConfig({
     tailwindcss(),
     vue(),
     vueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Food Reminder',
+        short_name: 'Food Reminder',
+        description: '賞味期限を簡単に記録＆管理',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        icons: [
+          {
+            src: '/icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    })
   ],
   resolve: {
     alias: {
