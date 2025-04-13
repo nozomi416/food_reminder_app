606c76a7e83b0a64f45fe51252ec80c0c50e86d1
n.takahashi <dkhdff.0415@gmail.com>
2025-04-13 23:29:01 +0900

    PWA対応

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
